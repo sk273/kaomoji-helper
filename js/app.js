9eca0b121143811701028e769bfce40ef076baf1
Seon Kang <seon.kang@gmail.com>
2017-08-23 17:37:04 -0400

    Add text replacement functionality in inject.js

--- js/app.js
+++ js/app.js
@@ -2,11 +2,10 @@ var app = angular.module('popup', []);
 
 app.controller('MainCtrl', function($scope) {
 
-        $scope.test = [1,2];
-
         $scope.kaomojis = [
                 {"kao" : "(╯°□°）╯︵ ┻━┻", "id" : "1","name" : "tableflip" },
-                {"kao" : "┬─┬ノ( º _ ºノ)" , "id" : "2","name" : "tablefix" }
-                //{"kao" : "¯\\_(ツ)_/¯" ,"id" : "3","name" : "shrug" }
+                {"kao" : "┬─┬ノ( º _ ºノ)" , "id" : "2","name" : "tablefix" },
+                {"kao" : "¯\\_(ツ)_/¯" ,"id" : "3","name" : "shrug" }
         ];
+
 });
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,42 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+var SPECIAL_KEY = ';';
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+document.addEventListener('keyup', function(e) { 
+        if (e.keyCode == 186) { // if semi-colon gets pressed... 59 SHOULD BE REPLACED WITH VARIABLE
+                var active = document.activeElement;
+                var caretPosition = active.selectionStart; // +1 is needed because 
+                var text = active.value;
+                var right = caretPosition - 1; // 
 
-	}
-	}, 10);
+                console.log(text);
+
+                var left = findSpecial(-1, right - 1, text);
+                if (left === -1) {
+                        left = right;
+                        right = findSpecial(1, left + 1, text);
+                        console.log("not found");
+                }
+
+                if (left >= 0 && right >= 0) {
+
+                        console.log("left: " + left);
+                        console.log("right: " + right);
+
+                        var kaomoji = "KAOMOJI";
+
+                        active.value = text.substring(0, left) + kaomoji + text.substring(right + 1);
+
+                        active.focus();
+                        active.selectionStart = left + kaomoji.length + 1;
+                        active.selectionEnd = left + kaomoji.length + 1;  
+                }              
+        }
 });
+
+var findSpecial = function(direction, start, text) {
+        while (start < text.length && start >= 0) {
+                if (text.charAt(start) === SPECIAL_KEY) return start;
+                else if (text.charAt(start) === ' ') break;
+                start += direction;
+        }
+        return -1;
+};
